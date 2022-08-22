e430e1f27c9d983de0d4a1b877e2de189edb007a
ardiantrik <ardiantrik@gmail.com>
2022-08-22 13:59:34 +0700

    initial commit sauceDemo automation

---
+++ cypress/e2e/sauceDemo/loginPage.cy.js
@@ -0,0 +1,39 @@
+/// <reference types="cypress"/>
+
+import LoginPage from "../../pages/sauceDemo/LoginPage";
+
+describe('Login Page Test', () => {
+    let loginPage = new LoginPage()
+
+    beforeEach(() => {
+        cy.visit('http://saucedemo.com')
+    })
+
+    it('visit web', () => {
+        
+    })
+
+    // it('Failed Login: Empty Username and Password', () => {
+    //     loginPage.loginButton().click().wait(1000)
+
+    //     //Assert
+    //     loginPage.alertInvalid().should('be.visible')
+    // })
+
+    // it('Failed Login: Fill Username Only', () => {
+    //     loginPage.userNameTextField().type('Ardian_K315')
+    //     loginPage.loginButton().click().wait(1000)
+
+    //     //Assert
+    //     loginPage.alertInvalid().should('be.visible')
+    // })
+
+    // it('Failed Login: Fill All, but Wrong Password', () => {
+    //     loginPage.userNameTextField().type('Ardian_K315')
+    //     loginPage.passwordTextField().type('123')
+    //     loginPage.loginButton().click().wait(1000)
+
+    //     //Assert
+    //     loginPage.alertInvalid().should('be.visible')
+    // })
+  })
